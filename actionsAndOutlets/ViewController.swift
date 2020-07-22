19258b0eae35e59860680c73cfbc511979bf0173
Hunter Malinowski <huntermalinowski@gmail.com>
2020-07-22 11:24:08 -0700

    Initial Commit

---
+++ actionsAndOutlets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  actionsAndOutlets
+//
+//  Created by Apple on 7/22/20.
+//  Copyright © 2020 Hunter Malinowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
